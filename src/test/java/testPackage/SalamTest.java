39ee1c33ae17338e738ff929e534f018dbe0ff69
mehdi-touil <elmehditouil19@gmail.com>
2022-10-20 08:50:41 +0100

    first commit

---
+++ src/test/java/testPackage/SalamTest.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+
+public class SalamTest {
+	
+   @Test
+   public void testMethod() {
+	     System.out.println("salam alaykom");
+   }
+}
